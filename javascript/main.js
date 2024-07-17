c36b285d48303c6b33b0b186b19942989cfa8f81
Colby Jensen <endergdawg@gmail.com>
2024-07-17 10:56:42 -0600

    fix square to use the multiply function

--- javascript/main.js
+++ javascript/main.js
@@ -8,64 +8,64 @@ document.getElementById("addButton").addEventListener("click", () => {
     const x = Number(document.getElementById("input1").value);
     const y = Number(document.getElementById("input2").value);
 
-    const results = add(x, y)
-    alert(`${x} + ${y} = ${results}`)
+    const results = add(x, y);
+    alert(`${x} + ${y} = ${results}`);
 })
 
 function add(x, y){
 
-    const z = x + y
+    const z = x + y;
 
-    console.log(z)
-    return z
+    console.log(z);
+    return z;
 }
 
 document.getElementById("multButton").addEventListener("click", () => {
     const x = Number(document.getElementById("input1").value);
     const y = Number(document.getElementById("input2").value);
 
-    const results = multiply(x, y)
-    alert(`${x} * ${y} = ${results}`)
+    const results = multiply(x, y);
+    alert(`${x} * ${y} = ${results}`);
 })
 
 
 function multiply(x, y){
-    const z = x * y
+    const z = x * y;
 
-    console.log(z)
-    return z
+    console.log(z);
+    return z;
 }
 
 
 document.getElementById("squareButton").addEventListener("click", () => {
     const x = Number(document.getElementById("input1").value);
 
-    const results = square(x)
-    alert(`${x} * ${x} = ${results}`)
+    const results = square(x);
+    alert(`${x} * ${x} = ${results}`);
 })
 
 function square(x){ 
-    const z = x * x
-    console.log(z)
-    return z
+    const z = multiply(x, x);
+    console.log(z);
+    return z;
 }
 
 document.getElementById("addSquaresButton").addEventListener("click", () => {
     const x = Number(document.getElementById("input1").value);
     const y = Number(document.getElementById("input2").value);
 
-    const results = addSquares(x, y)
-    alert(`(${x} * ${x}) + (${y} * ${y}) = ${square(x)} + ${square(y)} = ${results}`)
+    const results = addSquares(x, y);
+    alert(`(${x} * ${x}) + (${y} * ${y}) = ${square(x)} + ${square(y)} = ${results}`);
 })
 
 function addSquares(x, y){
-    const a = square(x)
-    const b = square(y)
+    const a = square(x);
+    const b = square(y);
 
-    const z = a + b
+    const z = a + b;
 
-    console.log(z)
-    return z
+    console.log(z);
+    return z;
 }
 
 
